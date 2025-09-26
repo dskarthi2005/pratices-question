7e7e79c8d3c78bb24aac88729ad6730f0680831d
dskarthi2005 <dskarthi2005@gmail.com>
2025-09-26 18:55:29 +0530

    Even or Odd problem

---
+++ evenorodd.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class evenorodd {
+
+public static void main(String[]args){
+    Scanner val=new Scanner(System.in);
+    int value=val.nextInt();
+    if(value%2==0){
+        System.out.print("Even");
+    }else{
+        System.out.print("odd");
+    }
+}
+    
+}
